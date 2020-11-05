a44ce9e105e6eaab8339bebdc67f9053e5bcac97
EvgenyEmets <e-evgeny99@ya.ru>
2020-11-06 01:22:28 +0300

    Add Temp. regulation and CLI

--- Task_2/main.cpp
+++ Task_2/main.cpp
@@ -12,7 +12,7 @@
 #include "simulation.h"
 #include "csv_mod.h"
 
-void func (Shed_Len *SInit, Mutation_R *MInit, Boltzmann *TInit, Shed_Len **Res, int thr) {
+void funcB (Shed_Len *SInit, Mutation_R *MInit, Boltzmann *TInit, Shed_Len **Res, int thr) {
     Shed_Len *S = dynamic_cast<Shed_Len*>(SInit->cpObj());
     Boltzmann *T = dynamic_cast<Boltzmann *>(TInit->cpObj());
     Mutation_R *M = dynamic_cast<Mutation_R *>(MInit->cpObj());
@@ -22,7 +22,25 @@ void func (Shed_Len *SInit, Mutation_R *MInit, Boltzmann *TInit, Shed_Len **Res,
     delete M;
 }
 
+void funcC (Shed_Len *SInit, Mutation_R *MInit, Cauchy *TInit, Shed_Len **Res, int thr) {
+    Shed_Len *S = dynamic_cast<Shed_Len*>(SInit->cpObj());
+    Cauchy *T = dynamic_cast<Cauchy *>(TInit->cpObj());
+    Mutation_R *M = dynamic_cast<Mutation_R *>(MInit->cpObj());
+    SimAnn A(S, T, M);
+    *Res = dynamic_cast<Shed_Len*>(A.Simulate());
+    delete T;
+    delete M;
+}
 
+void funcS (Shed_Len *SInit, Mutation_R *MInit, Simple *TInit, Shed_Len **Res, int thr) {
+    Shed_Len *S = dynamic_cast<Shed_Len*>(SInit->cpObj());
+    Simple *T = dynamic_cast<Simple *>(TInit->cpObj());
+    Mutation_R *M = dynamic_cast<Mutation_R *>(MInit->cpObj());
+    SimAnn A(S, T, M);
+    *Res = dynamic_cast<Shed_Len*>(A.Simulate());
+    delete T;
+    delete M;
+}
 
 int main() {
     std::vector<Work> W;
@@ -31,20 +49,35 @@ int main() {
     Shed_Len* S;
     S = new Shed_Len(NWork, NProc, W);
     Mutation_R M;
-    Boltzmann T(100);
+    //
+    int TM, ST;
+    std::cout << "Choose temperature mode and start temperature (1 - Boltzmann, 2 - Cauchy, else Simple mode): " << std::endl;
+    std::cin >> TM >> ST;
+    Boltzmann TB(ST);
+    Cauchy TC(ST);
+    Simple TS(ST);
     int Stop = 0;
-    //SimAnn A(S, &T, &M);
-    //Shed_Len *Ans = dynamic_cast<Shed_Len*>(A.Simulate());
-    //Ans->printSol();
-    //delete Ans;
-    int NTread = 4;
+    std::cout << "Enter the number of threads: " << std::endl;
+    int NTread;
+    std::cin >> NTread;
     std::vector<std::thread> ThreadVector;
     int BestMetric = S->getCritVal();
     while (Stop < 10) {
         std::vector<Shed_Len*> Buffer(NTread);
-        for (int i = 0; i < NTread; i++) {
-            ThreadVector.emplace_back(func, S, &M, &T, &(Buffer[i]), i);
+        if (TM == 1) {
+            for (int i = 0; i < NTread; i++) {
+                ThreadVector.emplace_back(funcB, S, &M, &TB, &(Buffer[i]), i);
+            }
+        } else if (TM == 2) {
+            for (int i = 0; i < NTread; i++) {
+                ThreadVector.emplace_back(funcC, S, &M, &TC, &(Buffer[i]), i);
+            }
+        } else {
+            for (int i = 0; i < NTread; i++) {
+                ThreadVector.emplace_back(funcS, S, &M, &TS, &(Buffer[i]), i);
+            }
         }
+
         for (int i = 0; i < NTread; i++) {
             ThreadVector[i].join();
         }
